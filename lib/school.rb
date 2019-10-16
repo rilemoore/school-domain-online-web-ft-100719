84c3e8bfe06f4b432ad8bad6e03864a8e72b1639
Riley Moore <rilemoore@gmail.com>
2019-10-16 20:53:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -28,3 +28,11 @@ class School
   end
   #self.roster == @roster
 end
+
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
